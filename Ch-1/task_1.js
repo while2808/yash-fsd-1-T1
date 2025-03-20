8304d2b8b7aabd188eaa369f441fbeed8bae8c0f
Romil <goyaniyash120@gmail.com>
2025-03-20 12:40:39 +0530

    Delete Ch-1 directory

--- Ch-1/task_1.js
+++
@@ -1,9 +0,0 @@
-// print "give again" using object & string concept
-const user = {
-    "name":["ABC","DEF","GHI"],
-    "age":18,
-    "course":["FSD-1","DE","FSD-2"],
-    "adress":["T1","T2",{"t3":"Give Again"}]
-}
-
-console.log(user.adress[2].t3) // Give again
