97f4b869d535dbd3ae3292e3341dc8341b4cfc1b
Romil <goyaniyash120@gmail.com>
2025-04-05 10:40:01 +0530

    Add files via upload

---
+++ 10_task_7.js
@@ -0,0 +1,19 @@
+// o/p : John has BMW-x3 at age of 30.
+myobj = {
+    "name":"John",
+    "age":30,
+    "cars" : [
+        {"name":"Ford","modals":["Fusta","Focus","Mustang"]},
+        {"name":"BMW","modals":["320","X3","X5"]},
+        {"name":"Fiat","modals":["500","Panda"]}
+    ]
+}
+
+
+{/* perform tasks
+    1.)write a object in  s2.txt
+        {d:{a:10,b:20,c:[30,10]}}
+    2.) read data & perform tasks
+        a.)add a & b
+        b.)sub 2nd element c & b.
+*/}
